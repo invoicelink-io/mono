8e7b690c3185c901440f5ee0b4b1ae2073e08783
Ratul Maharaj <56479869+RatulMaharaj@users.noreply.github.com>
2025-04-03 02:21:24 +0200

    feat: ✨ Add VAT number display to invoice addresses

--- apps/app/src/lib/components/invoice/Address.svelte
+++ apps/app/src/lib/components/invoice/Address.svelte
@@ -4,12 +4,18 @@
 	export let name: string;
 	export let data: Address;
 	export let align = 'text-left';
+	export let vatNumber: string | null = null;
 </script>
 
 <div class={twMerge('flex w-full flex-col leading-5', align)}>
 	<span>{name}</span>
+	<br />
 	<span>{data.line1}</span>
 	<span>{data.line2 ?? ''}</span>
 	<span>{data.line3 ?? ''}</span>
 	<span>{data.postalCode}</span>
+	<br />
+	{#if vatNumber}
+		<span>VAT No. {vatNumber}</span>
+	{/if}
 </div>
